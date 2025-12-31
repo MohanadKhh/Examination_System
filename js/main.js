61c8dc3b0606304289d6c2b113f21b83443293fe
AmrKhaledAK47 <2002amrkhaled@gmail.com>
2025-12-31 18:33:46 +0200

    Update file paths for assets and scripts to use absolute URLs; enhance consistency across HTML files.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 guardRoute();
-if(getCurrentPage() === '') {
+
+// handles bfcache
+window.addEventListener('pageshow', function (event) {
+    if (event.persisted) {
+        if (!guardRoute()) {
+            document.body.classList.add('opacity-0');
+        }
+    }
+});
+
+if (isProtectedRoute()) {
+    window.addEventListener('unload', function () { });
+}
+
+if (getCurrentPage() === '') {
     to('/register/');
 }

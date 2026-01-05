d80e3c51fc61eed967a7d140bd7976061ff4e448
Mohanad Khaled Hassan <mohanedkkhaled@gmail.com>
2026-01-05 12:07:11 +0200

    Last update isa 1/5/2025 12:07

--- js/result.js
+++ js/result.js
@@ -1,7 +1,7 @@
 showPage();
 let student = JSON.parse(localStorage.getItem("currentUser"));
-let grade = student?.grade?.correct || 0;
-const questions = student?.total || 10;
+let grade = student.grade.correct || 0;
+const questionsLength = student.grade.total || 10;
 
 if (student) {
     document.getElementById("studentName").innerText =
@@ -15,19 +15,55 @@ signOutBtn.addEventListener('click', function () {
 
 const correctQu = document.querySelector(".correct__counts p")
 const inCorrectQu = document.querySelector(".incorrect__counts p")
-const progress = document.querySelector(".radial-progress")
+const scoreProgress = document.querySelector(".radial-progress")
+const valueText = document.getElementById("scoreValue");
+
+const titleMsg = document.querySelector(".title__message")
+const titleStatus = document.querySelector(".title__status")
 
 correctQu.innerText = grade
-inCorrectQu.innerText = questions - grade
+inCorrectQu.innerText = questionsLength - grade
 
-let percent = +grade * 10;
-progress.querySelector("div").innerText = percent + "%"
-progress.style.setProperty("--value", percent);
-progress.setAttribute("aria-valuenow", percent);
+let percent = +grade * 10
 
 if (percent < 60) {
-    progress.classList.add("text-[#dc2828]")
+    titleMsg.innerText = "Keep Practicing!"
+    titleStatus.innerText = "Fail"
+    titleStatus.classList.add("text-[#dc2828]")
 }
 else {
-    progress.classList.add("text-[#28af60]")
+    titleMsg.innerText = "Great keep going!"
+    titleStatus.innerText = "Success"
+    titleStatus.classList.add("text-[#28af60]")
 }
+
+if (percent < 60) {
+    scoreProgress.classList.add("text-[#dc2828]")
+}
+else {
+    scoreProgress.classList.add("text-[#28af60]")
+}
+
+function animateRadialProgress(target, duration = 2000) {
+  let startTime = null;
+
+  function step(timestamp) {
+    if (!startTime) startTime = timestamp;
+
+    const progress = Math.min((timestamp - startTime) / duration, 1);
+    const current = Math.floor(progress * target);
+
+    // update UI
+    scoreProgress.style.setProperty("--value", current);
+    scoreProgress.setAttribute("aria-valuenow", current);
+    valueText.innerText = current + "%";
+
+    if (progress < 1) {
+      requestAnimationFrame(step);
+    }
+  }
+
+  requestAnimationFrame(step);
+}
+
+animateRadialProgress(percent)
